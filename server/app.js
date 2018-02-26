3e7193d5013f4b54f1e25bfb4ff0956ce7d7bed6
Ricky Ng-Adam <rngadam@gmail.com>
2018-02-26 21:37:40 +0800

    bump up entity size

--- server/app.js
+++ server/app.js
@@ -7,10 +7,14 @@ const bodyParser = require('body-parser')
 const app = express()
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json({
+    limit: "5mb"
+}))
 
 mountRoutes(app)
 
