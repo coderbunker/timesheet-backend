0b2cfc91ed0932f29feb192ea28fbe4ed291605b
Ricky Ng-Adam <rngadam@gmail.com>
2018-02-28 18:43:54 +0800

    filter out bad element and get additional properties out

--- server/sql/incoming.entry.sql
+++ server/sql/incoming.entry.sql
@@ -1,10 +1,22 @@
-CREATE VIEW incoming.entry AS
+CREATE OR REPLACE VIEW incoming.raw_entry AS
+	SELECT
+		json_array_elements(DATA) AS element,
+		"timezone",
+		id,
+		name
+	FROM
+		incoming.snapshot
+	;
+					
+CREATE OR REPLACE VIEW incoming.entry AS
 	SELECT
 		DATE + START AS start_datetime,
 		DATE + stop AS stop_datetime,
 		stop - START AS duration,
 		resource,
-		project_id
+		project_id,
+		taskname,
+		activity
 	FROM
 		(
 			SELECT
@@ -18,15 +30,15 @@ CREATE VIEW incoming.entry AS
 					ELEMENT ->> 'stop'
 				)::INTERVAL AS stop,
 				ELEMENT ->> 'resource' AS resource,
-				id AS project_id
-			FROM (
-				SELECT
-					json_array_elements(DATA) AS element,
-					"timezone",
-					id,
-					name
-				FROM
-					incoming.snapshot
-			) AS ELEMENT
+				id AS project_id,
+				ELEMENT->>'task name' AS taskname,
+				ELEMENT->>'activity' AS activity
+			FROM incoming.raw_entry AS ELEMENT
+			WHERE
+				length(ELEMENT->>'start') > 0 AND
+				length(ELEMENT->>'stop') > 0 AND
+				length(ELEMENT->>'resource') > 0 AND
+				length(ELEMENT->>'date') > 0 
 		) AS converted
+
 	ORDER BY start_datetime;
