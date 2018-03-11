9824b6b96a48565ce66eb818b2bf3d4b373de216
Ricky Ng-Adam <rngadam@gmail.com>
2018-03-11 23:39:18 +0800

    a bit more stats and remove minimum hours

--- server/sql/postgraphql.sql
+++ server/sql/postgraphql.sql
@@ -19,12 +19,30 @@ DROP MATERIALIZED VIEW IF EXISTS postgraphql.organization;
 CREATE MATERIALIZED VIEW postgraphql.organization AS
 	SELECT 
 		organization.*,
-		now() AS last_refresh
+		now() AS last_refresh,
+		(SELECT max(last_update) FROM incoming.project) AS last_update
 	FROM report.organization;
 
-CREATE FUNCTION postgraphql.refresh_data() RETURNS void AS
+DROP FUNCTION postgraphql.refresh_data;
+
+CREATE FUNCTION postgraphql.refresh_data() RETURNS timestamptz AS
 $$
 	REFRESH MATERIALIZED VIEW postgraphql.organization;
+	SELECT last_refresh FROM postgraphql.organization; 
 $$ LANGUAGE SQL;
 
-SELECT postgraphql.refresh_data();
+SELECT * FROM postgraphql.refresh_data();
+
+
+-- DROP FUNCTION postgraphql.refresh_data_conditional;z
+--CREATE OR REPLACE FUNCTION postgraphql.refresh_data_conditional(OUT last_refresh timestamptz, OUT last_update timestamptz, OUT last_refresh_now timestamptz) AS
+--$$
+--BEGIN
+--	SELECT 
+--		organization.last_refresh,
+--		(CASE WHEN organization.last_refresh < (SELECT max(project.last_update) FROM incoming.project) THEN (select postgraphql.refresh_data()) ELSE organization.last_refresh END) FROM postgraphql.organization;	
+--END;
+--$$ LANGUAGE plpgsql;
+--
+--SELECT * FROM postgraphql.refresh_data_conditional();
+
