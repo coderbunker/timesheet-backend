9a19374f1edb55590c477bab29555cf05127e052
Ricky Ng-Adam <rngadam@gmail.com>
2018-04-25 17:04:35 +0800

    remove manual as to not confuse user

--- manual-import/postgraphql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./node_modules/.bin/postgraphql \
-	--disable-default-mutations \
-	--dynamic-json \
-	--schema dw \
-	--watch \
-	-c postgres://localhost/timesheet \
-	--cors \
-	--host 10.209.193.135
