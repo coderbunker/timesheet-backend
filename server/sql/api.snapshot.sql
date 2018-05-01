b5bab8af69cb3ab2b8d0b8f2b5948d9e59a8c36a
Ricky Ng-Adam <rngadam@gmail.com>
2018-05-01 14:19:06 +0800

    issue #17: two stage checking for warning reporting

--- server/sql/api.snapshot.sql
+++ server/sql/api.snapshot.sql
@@ -4,26 +4,47 @@ CREATE SCHEMA IF NOT EXISTS api;
 
 CREATE OR REPLACE VIEW api.warnings AS
 	(
-	SELECT
-		id,
-		doc::jsonb,
-		'incoming.snapshot' AS table_name,
-		'apptype should be present in doc' AS error
-	FROM incoming.snapshot
-	WHERE doc->>'apptype' IS NULL
+		SELECT
+			id,
+			'{}'::jsonb AS doc,
+			'incoming.snapshot' AS table_name,
+			'name should be present in doc' AS error
+		FROM incoming.snapshot
+		WHERE doc->>'name' IS NULL
 	)
 	UNION
 	(
-	SELECT
-		id,
-		doc::jsonb,
-		'incoming.snapshot' AS table_name,
-		'category should be present in doc' AS error
-	FROM incoming.snapshot
-	WHERE doc->>'category' IS NULL
+		SELECT
+			id,
+			'{}'::jsonb AS doc,
+			'incoming.snapshot' AS table_name,
+			'timezone should be present in doc' AS error
+		FROM incoming.snapshot
+		WHERE doc->>'timezone' IS NULL
+	)
+	UNION
+	(
+		SELECT
+			id,
+			'{}'::jsonb AS doc,
+			'incoming.snapshot' AS table_name,
+			'apptype should be present in doc' AS error
+		FROM incoming.snapshot
+		WHERE doc->>'apptype' IS NULL
+	)
+	UNION
+	(
+		SELECT
+			id,
+			'{}'::jsonb AS doc,
+			'incoming.snapshot' AS table_name,
+			'category should be present in doc' AS error
+		FROM incoming.snapshot
+		WHERE doc->>'category' IS NULL
 	)
 	;
-	
+
+ 
 CREATE OR REPLACE FUNCTION api.snapshot(text, json)
 RETURNS SETOF api.warnings AS
 $func$
@@ -33,6 +54,11 @@ BEGIN
 	    ON CONFLICT(id) DO
 	      UPDATE SET doc = EXCLUDED.doc, ts = now() WHERE snapshot.id = EXCLUDED.id;
 	RETURN QUERY SELECT * FROM api.warnings WHERE id = $1;
+	IF NOT FOUND THEN
+		RETURN QUERY SELECT $1::text AS id, doc::jsonb, table_name, error 
+			FROM incoming.warnings 
+			WHERE doc->>'project_id' = $1;
+	END IF;
 END;
 $func$  LANGUAGE plpgsql;
 
--- server/sql/incoming/warnings.sql
+++ server/sql/incoming/warnings.sql
@@ -1,135 +1,141 @@
 CREATE OR REPLACE VIEW incoming.warnings AS
-	SELECT * FROM
 	(
-		(
-			SELECT
-				to_json(profile.*) AS doc,
-				'incoming.profile' AS table_name,
-				'email must be present for all profiles' AS error
-			FROM incoming.profile
-			WHERE email IS NULL
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(entry.*) AS doc,
-				'incoming.entry' AS table_name,
-				'time entry is negative' AS error
-			FROM incoming.entry
-			WHERE EXTRACT(EPOCH FROM (stop_datetime-start_datetime)) < 0
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(entry.*) AS doc,
-				'incoming.entry' AS table_name,
-				'no task name given' AS error
-			FROM incoming.entry
-			WHERE taskname IS NULL
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(entry.*) AS doc,
-				'incoming.entry' AS table_name,
-				'no activity name given' AS error
-			FROM incoming.entry
-			WHERE taskname IS NULL
-		)
-		UNION ALL
-		(
-			SELECT
-				format('{"project_id": "%s"}', project_id)::json AS doc,
-				'incoming.entry' AS table_name,
-				'taskname is on average longer than activity - reversed?' AS error
-			FROM incoming.entry
-			WHERE activity IS NOT NULL AND taskname IS NOT NULL
-			GROUP BY project_id
-			HAVING avg(length(taskname)) > avg(length(activity))
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(entry.*) AS doc,
-				'incoming.entry' AS table_name,
-				'resource is null or length 0' AS error
-			FROM incoming.entry
-			WHERE entry.resource IS NULL OR LENGTH(entry.resource) = 0
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(entry.*) AS doc,
-				'incoming.entry' AS table_name,
-				'resource does not match anything in people' AS error
-			FROM incoming.entry LEFT JOIN incoming.people ON(entry.resource = ANY(people.nicknames))
-			WHERE people.fullname IS NULL
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(people.*) AS doc,
-				'incoming.people' AS table_name,
-				'no email for a people entry' AS error
-			FROM incoming.people
-			WHERE people.email IS NULL
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(raw_people.*) AS doc,
-				'incoming.raw_people' AS table_name,
-				'no key rate specified' AS error
-			FROM incoming.raw_people
-			WHERE doc->>'rate' IS NULL
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(raw_people.*) AS doc,
-				'incoming.raw_people' AS table_name,
-				'no key rate specified' AS error
-			FROM incoming.raw_people
-			WHERE doc->>'rate' IS NULL
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(entry.*) AS doc,
-				'incoming.entry' AS table_name,
-				'duration is negative' AS error
-			FROM incoming.entry
-			WHERE EXTRACT (epoch FROM (stop_datetime-start_datetime)) < 0
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(entry.*) AS doc,
-				'incoming.entry' AS table_name,
-				'start_datetime or stop_datetime in future' AS error
-			FROM incoming.entry
-			WHERE start_datetime >= NOW() OR stop_datetime >= NOW()
-		)
-		UNION ALL
-		(
-			SELECT
-				to_json(entry_calendar.*) AS doc,
-				'incoming.entry_calendar' AS table_name,
-				'start_datetime or stop_datetime in future' AS error
-			FROM incoming.entry_calendar
-			WHERE start_datetime >= NOW() OR stop_datetime >= NOW()
-		)
-		UNION ALL
-		(
-			SELECT
-				(array_agg(to_json(people_project.*)))[1] AS doc,
-				'incoming.people_project' AS table_name,
-				format('multiple resource matches for resource " %s" email "%s" count "%s"', resource, email, count(*)) AS error
-			FROM incoming.people_project
-			GROUP BY project_id, email, resource
-			HAVING count(*) > 1
-		)
-	) AS all_errors
+		SELECT
+			to_json(profile.*) AS doc,
+			'incoming.profile' AS table_name,
+			'email must be present for all profiles' AS error
+		FROM incoming.profile
+		WHERE email IS NULL
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(entry.*) AS doc,
+			'incoming.entry' AS table_name,
+			'time entry is negative' AS error
+		FROM incoming.entry
+		WHERE EXTRACT(EPOCH FROM (stop_datetime-start_datetime)) < 0
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(entry.*) AS doc,
+			'incoming.entry' AS table_name,
+			'no task name given' AS error
+		FROM incoming.entry
+		WHERE taskname IS NULL
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(entry.*) AS doc,
+			'incoming.entry' AS table_name,
+			'no activity name given' AS error
+		FROM incoming.entry
+		WHERE taskname IS NULL
+	)
+	UNION ALL
+	(
+		SELECT
+			format('{"project_id": "%s"}', project_id)::json AS doc,
+			'incoming.entry' AS table_name,
+			'taskname is on average longer than activity - reversed?' AS error
+		FROM incoming.entry
+		WHERE activity IS NOT NULL AND taskname IS NOT NULL
+		GROUP BY project_id
+		HAVING avg(length(taskname)) > avg(length(activity))
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(entry.*) AS doc,
+			'incoming.entry' AS table_name,
+			'resource is null or length 0' AS error
+		FROM incoming.entry
+		WHERE entry.resource IS NULL OR LENGTH(entry.resource) = 0
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(entry.*) AS doc,
+			'incoming.entry' AS table_name,
+			'resource does not match anything in people' AS error
+		FROM incoming.entry LEFT JOIN incoming.people ON(entry.resource = ANY(people.nicknames))
+		WHERE people.fullname IS NULL
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(people.*) AS doc,
+			'incoming.people' AS table_name,
+			'no email for a people entry' AS error
+		FROM incoming.people
+		WHERE people.email IS NULL
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(raw_people.*) AS doc,
+			'incoming.raw_people' AS table_name,
+			'no key rate specified' AS error
+		FROM incoming.raw_people
+		WHERE doc->>'rate' IS NULL
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(raw_people.*) AS doc,
+			'incoming.raw_people' AS table_name,
+			'no key rate specified' AS error
+		FROM incoming.raw_people
+		WHERE doc->>'rate' IS NULL
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(entry.*) AS doc,
+			'incoming.entry' AS table_name,
+			'duration is negative' AS error
+		FROM incoming.entry
+		WHERE EXTRACT (epoch FROM (stop_datetime-start_datetime)) < 0
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(entry.*) AS doc,
+			'incoming.entry' AS table_name,
+			'duration is longer than 14 hours' AS error
+		FROM incoming.entry
+		WHERE EXTRACT (hour FROM (stop_datetime-start_datetime)) >= 14
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(entry.*) AS doc,
+			'incoming.entry' AS table_name,
+			'start_datetime or stop_datetime in future' AS error
+		FROM incoming.entry
+		WHERE start_datetime >= NOW() OR stop_datetime >= NOW()
+	)
+	UNION ALL
+	(
+		SELECT
+			to_json(entry_calendar.*) AS doc,
+			'incoming.entry_calendar' AS table_name,
+			'start_datetime or stop_datetime in future' AS error
+		FROM incoming.entry_calendar
+		WHERE start_datetime >= NOW() OR stop_datetime >= NOW()
+	)
+	UNION ALL
+	(
+		SELECT
+			(array_agg(to_json(people_project.*)))[1] AS doc,
+			'incoming.people_project' AS table_name,
+			format('multiple resource matches for resource " %s" email "%s" count "%s"', resource, email, count(*)) AS error
+		FROM incoming.people_project
+		GROUP BY project_id, email, resource
+		HAVING count(*) > 1
+	)
 	;
 
 
