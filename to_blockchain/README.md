abca6c7b7a67d90bb48c0ba25495f2a8c6ae0824
arseny <arseny.jin@ya.ru>
2018-05-10 19:52:24 +0800

    Update readme and data_storage_contract.sol

--- to_blockchain/README.md
+++ to_blockchain/README.md
@@ -1,5 +1,37 @@
 # timesheet-backend/to_blockchain
 
-Basic "skeleton" implementation of storage smart contract
+Basic "skeleton" implementation of storage smart contract + simple interface to store data on smart contract
 
-For test purposes copy and paste code to http://remix.ethereum.org/
+To test the following code you need Node.js and NPM
+
+run
+
+```bash
+npm install -g ethereumjs-testrpc
+```
+
+then start it
+
+```bash
+testrpc
+```
+In different terminal window in to_blockchain directory run
+
+```bash
+npm init
+```
+
+```bash
+npm install ethereum/web3.js --save
+```
+
+copy code from data_storage_contract.sol to http://remix.ethereum.org/ compile code and deploy it to "Web3 Provider" of your test network
+
+change 
+
+```
+var data_storage = data_storage_contract.at("YOU SMART CONTRACT'S ADRESS");
+```
+run index.html
+
+now you can store data in smart contract in the format : address, string, string
--- to_blockchain/data_storage_contract.sol
+++ to_blockchain/data_storage_contract.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.23;
 contract data_storage_contract {
     
     struct Freelancer {
-        bytes32 id; //freelancer id hash
-        bytes32 name; //freelancer's name
+        string id; //freelancer id hash
+        string name; //freelancer's name
         address projectAdress; //project's ethereum address
         uint32 hoursSpent; 
         uint32 tasksCompleted; 
@@ -12,8 +12,8 @@ contract data_storage_contract {
 
     // struct for storing projects related information
     struct Project {  
-        bytes32 id;  // project id hash
-        bytes32 name; //project name
+        string id;  // project id hash
+        string name; //project name
     }
     
     mapping (address => Project) projects; // bind project's address to Project type struct
@@ -22,7 +22,7 @@ contract data_storage_contract {
     mapping (address => Freelancer) freelancers; // bind freelancer's address to freelancer type struct
     address[] public freelancerAccs;
      
-    function addProject(address _address, bytes32 _id, bytes32 _name) public { // TODO: add msg.sender
+    function addProject(address _address, string _id, string _name) public { 
         Project storage project = projects[_address];
         
         project.id = _id;
@@ -35,13 +35,13 @@ contract data_storage_contract {
         return projectAccs;
     }
  
-    function getProject(address _address) view public returns(bytes32, bytes32){ // get data for a specific project
+    function getProject(address _address) view public returns(string, string){ // get data for a specific project
         return (projects[_address].id, projects[_address].name);
     }
     
     function addFreelancer(address _address, 
-                           bytes32 _id, 
-                           bytes32 _name, 
+                           string _id, 
+                           string _name, 
                            address _projectAdress, 
                            uint32 _hoursSpent, 
                            uint32 _tasksCompleted) public{
@@ -58,7 +58,7 @@ contract data_storage_contract {
         return freelancerAccs;
     }
     
-    function getFreelancer(address _address) view public returns(bytes32, bytes32,address, uint32, uint32){ // get data for a spiciefic freelancer
+    function getFreelancer(address _address) view public returns(string, string, address, uint32, uint32){ // get data for a spiciefic freelancer
         return (freelancers[_address].id, 
                 freelancers[_address].name, 
                 freelancers[_address].projectAdress, 
