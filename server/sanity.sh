b2cf2e169932baa1ed43442f848f0a9c264438a6
Ricky Ng-Adam <rngadam@gmail.com>
2018-07-22 08:34:05 +0800

    issue #26: support db url, actually addon postgresql, sanity check

--- server/sanity.sh
+++ server/sanity.sh
@@ -3,9 +3,14 @@ if [ -z "$1" ]; then
 	echo "please provide dbname"
 	exit 1
 fi
+
 if [[ $1 == postgres://* ]]; then
 	DBNAME=$1
 else
 	DBNAME="postgres://localhost/$1"
 fi
-psql -f sql/900-psql-testsuite.sql $DBNAME
+
+echo "Using $DBNAME"
+
+psql -f sql/utils/show_extensions.sql $DBNAME
+
--- server/setup.sh
+++ server/setup.sh
@@ -3,11 +3,19 @@ if [ -z "$1" ]; then
 	echo "please provide dbname"
 	exit 1
 fi
-DBNAME=$1
 
-psql -f sql/utils/disconnect.sql postgres://localhost/$DBNAME
-psql -c "SELECT utils.disconnect('$DBNAME');" postgres://localhost/$DBNAME
-psql -v "ON_ERROR_STOP=1" -b -1 -e -f sql/PSQL.sql postgres://localhost/$DBNAME
+# if matches will return the smallest prefix thus not equal
+if [[ $1 == postgres://* ]]; then
+	DBNAME=$1
+else
+	DBNAME="postgres://localhost/$1"
+fi
+
+echo "Using $DBNAME"
+
+psql -f sql/utils/disconnect.sql $DBNAME
+psql -c "SELECT utils.disconnect('$DBNAME');" $DBNAME
+psql -v "ON_ERROR_STOP=1" -b -1 -e -f sql/PSQL.sql $DBNAME
 #echo "# you can now run npm start to start the GraphQL endpoints"
 #echo "# run:"
 #echo "npm start"
